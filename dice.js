da5d2639f15998662d0e65d28c5d055aa6d8feb1
mandmtherapy <davidlewisvalentine@gmail.com>
2019-06-18 07:09:20 -0700

    fix #8

--- dice.js
+++ dice.js
@@ -15,7 +15,7 @@ roll = (formattedArg, author = 'You') => {
     rolls.forEach((val, index) => {
         if (isD10) {
             // D10 is a percent die
-            rolls[index] = _.random(0, 10);
+            rolls[index] = _.random(0, 9);
         } else {
             rolls[index] = _.random(1, formattedArg.type);
         }
